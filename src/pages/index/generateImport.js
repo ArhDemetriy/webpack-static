6cc24de31b4e902c8f3983c4195f3d8d3cb72df2
ArhDemetriy <ArhDemetriy@GMail.com>
2021-01-23 23:22:10 +0800

    пытаюсь сделать рекурсивные промисы

--- src/pages/index/generateImport.js
+++ src/pages/index/generateImport.js
@@ -4,57 +4,126 @@
   const fs = require('fs')
   const fsPromises = fs.promises
   // input importsName
-  const nameImports = (() => {
-    const nameImports = require(path.resolve(__dirname, 'import.json'))
-    if (!Array.isArray(nameImports)) throw new Error('import.json is not array')
-    return Array.from(new Set(nameImports))
-  })()
+  // const nameImports = (() => {
+  //   const nameImports = require(path.resolve(__dirname, 'import.json'))
+  //   if (!Array.isArray(nameImports)) throw new Error('import.json is not array')
+  //   return Array.from(new Set(nameImports))
+  // })()
   const componentsPath = 'components'
   // folders path
   const simplePath = `${componentsPath}/simple`
   const complicatedPath = `${componentsPath}/complicated`
   // lists for exist folder name
-  const simpleImportNameList = new Set()
+  const notCheckedSimpleImportNameList = new Set()
   const complicatedImportNameList = new Set()
-  const isExistsPromise = (folder, name) =>
-    fsPromises.access(path.resolve(`src/${folder}/${name}`), fs.constants.F_OK)
-  const noExistsNames =
-    (await Promise.all(nameImports
-                        .map(name => isExistsPromise(complicatedPath, name)
-                          .then(
-                            exists => {
-                              complicatedImportNameList.add(name)
-                              isExistsPromise(complicatedPath, `${name}/import.json`)
-                                .then(
-                                  exists => {
-                                    const nameImports = (() => {
-                                      const nameImports = require(path.resolve(`${complicatedPath}/${name}`, 'import.json'));
-                                      if (Array.isArray(nameImports))
-                                        return Array.from(new Set(nameImports));
-                                    })()
+  const isExistsPromise = absolutePath =>
+    fsPromises.access(path.resolve(`src/${absolutePath}`), fs.constants.F_OK);
 
-                                    simpleImportNameList.add(name);
-                                  },
-                                  noExists => {})
+  const parseImports = nameImports =>
+    Promise.resolve(nameImports)
+      .then(nameImports => {
+        console.log(nameImports);
+        const temp = new Set(nameImports);
+        complicatedImportNameList.forEach(value => temp.delete(value));
+        notCheckedSimpleImportNameList.forEach(value => temp.delete(value));
+        if (temp.size <= 0) throw new Error('nameImports.size <= 0 or nameImports is not iterable');
+        return Array.from(temp);
+      })
+      .then(nameImports => {
+        console.log(nameImports);
+        const temp = Promise.all(nameImports.map(name => {
+          isExistsPromise(`${complicatedPath}/${name}`)
+            .catch(() => Promise.reject(notCheckedSimpleImportNameList.add(name),console.log('add simple')))
+            .then(exists => {
+              complicatedImportNameList.add(name);
+              return isExistsPromise(`${complicatedPath}/${name}/import.json`);
+            })
+            .then(() => require(path.resolve(`src/${complicatedPath}/${name}`, 'import.json')))
+            // .catch(err => Promise.reject(err))
+            .then(additionalImports => parseImports(additionalImports))
+            .catch(err => err)
+            // .finally(() => {
+            //   console.log(complicatedImportNameList);
+            //   console.log(notCheckedSimpleImportNameList);
+            // })
+        }));
+        console.log('name');
 
-                            },
-                            noExists => isExistsPromise(simplePath, name)
-                              .then(
-                                exists => { simpleImportNameList.add(name) },
-                                noExists => name))))
-    ).filter(v => v)
+        // return Promise.allSettled(temp);
+        return temp
+      })
+      .catch(err => err)
+      // .finally(() => {
+      //   console.log(complicatedImportNameList);
+      //   console.log(notCheckedSimpleImportNameList);
+      // })
 
 
 
 
-  console.log('\ncomplicated\n', complicatedImportNameList)
-  console.log('\nsimple\n', simpleImportNameList)
-  console.log('\nnoExistsNames\n', noExistsNames)
 
+      // // const checkingNameImportsPromises = nameImports.map((name, _, array) =>
+      // const checkingNameImportsPromises = []
+      // for (let i = 0; true; i++){
+        //   console.log(nameImports.length);
+  //   if (i >= nameImports.length) break;
+  //   const name = nameImports[i];
+  //   checkingNameImportsPromises.push(isExistsPromise(`${complicatedPath}/${name}`)
+  //     .catch(() => Promise.reject(notCheckedSimpleImportNameList.add(name)))
+  //     .then(exists => {
+    //       complicatedImportNameList.add(name);
+    //       return isExistsPromise(`${complicatedPath}/${name}/import.json`);
+    //     })
+    //     .then(existsadditionalImports => {
+  //       // console.log(`\n есть дополнительные импорты\n ${path.resolve(`src/${complicatedPath}/${name}`, 'import.json')}`);
+  //       const temp = new Set(require(path.resolve(`src/${complicatedPath}/${name}`, 'import.json')));
+  //       if (temp.size <= 0) throw new Error(`${complicatedPath}/${name}/import.json empty or no iterable`);
+  //       // console.log('temp: ', temp);
+  //       return temp;
+  //     })
+  //     .then(additionalImports => {
+  //       // notCheckedSimpleImportNameList.add(additionalImports);
+  //       console.log('additionalImports', additionalImports);
+  //       console.log('array', nameImports);
+  //       nameImports.forEach(value => additionalImports.delete(value));
+  //       console.log('additionalImports', additionalImports);
+  //       if (additionalImports.size >= 1)
+  //         nameImports.push(...additionalImports)
+  //       console.log('array', nameImports);
+  //       console.log('****************');
+  //       return Promise.reject(`${additionalImports.size} additionalImports`)
+  //     })
+  //     .catch(e => e));
+  // }
+  const parseImportsPromise = parseImports(require(path.resolve(__dirname, 'import.json')))
+    .finally(() => {
+      console.log(complicatedImportNameList);
+      console.log(notCheckedSimpleImportNameList);
+
+  })
+  await parseImportsPromise;
+  console.log(complicatedImportNameList);
+  console.log(notCheckedSimpleImportNameList);
+
+
+
+  // console.log(notCheckedSimpleImportNameList);
+  // console.log(complicatedImportNameList);
+
+  const simpleImportNameList = new Set()
+  const checkSimpleImportNameList = Promise.all(Array.from(notCheckedSimpleImportNameList).map(name =>
+    isExistsPromise(`${simplePath}/${name}`)
+      .then(simpleImportNameList.add(name), e => e)));
+
+  const includeKeyword = 'include '
+  let includePref = ''
+  if (complicatedImportNameList.size >= 1) {
+    includePref = `${includeKeyword}/${complicatedPath}/`
+    console.log(includePref + Array.from(complicatedImportNameList).join(`\n${includePref}`));}
+  await checkSimpleImportNameList;
   if (simpleImportNameList.size >= 1) {
-    const includePref = '\ninclude /'
-    console.log('include /' + Array.from(simpleImportNameList).join(includePref));
-  }
+    includePref = `${includeKeyword}/${simplePath}/`
+    console.log(includePref + Array.from(simpleImportNameList).join(`\n${includePref}`));}
 
 })()
 
