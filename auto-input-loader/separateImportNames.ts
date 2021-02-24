dd052e47ab59ed8dca9c5ec338ddf2a945567a5e
ArhDemetriy <ArhDemetriy@GMail.com>
2021-02-24 17:48:55 +0800

    ref переписываю ядро лоадера на классы add пишу отдельные функции для импортов из разного количества каталогов

--- auto-input-loader/separateImportNames.ts
+++ auto-input-loader/separateImportNames.ts
@@ -1,17 +1,165 @@
 /** @type {import('node')} */
+import { AutoInputOptions } from './types'
 import path = require('path')
-import { constants as fsConstsnts, promises as fsPromises } from 'fs'
+import { constants as fsConstants, promises as fsPromises } from 'fs'
+import { setFlagsFromString } from 'v8'
 
+
+class FinderPathFromNames{
+  protected readonly startDirectory: string
+  protected readonly startFile: string
+  protected readonly nameImportsFile: string
+  // имена импортируемых файлов разделёные по их родительским каталогам взятым из sourcePaths
+  protected readonly nameLists: Map<String,Set<string>>
+  constructor(startFile: string, sourcePaths: AutoInputOptions['sourcePaths']) {
+    this.startFile = startFile
+    this.nameImportsFile = path.basename(this.startFile)
+    this.startDirectory = path.dirname(this.startFile)
+    this.nameLists = new Map(sourcePaths.map(sourcePath => [sourcePath, new Set()]))
+  }
+  protected readonly notCheckedNameList: Set<string> = new Set()
+  protected readonly additionalNamesList: Set<string> = new Set()
+  protected delDublicateFrom(nameList: Set<string>) {
+    for (let names of this.nameLists.values()) {
+      if (nameList.size <= 0) break;
+      names.forEach(name => nameList.delete(name))
+    }
+    return nameList
+  }
+  protected parseImportNames(from: string, name: string[]) {
+
+
+    const result = ''
+    return result
+  }
+  protected parseImports(nameImports: string[]) {
+    const result = Promise.resolve(nameImports)
+      .then(nameImports => {
+        const temp = this.delDublicateFrom(new Set(nameImports))
+        if (temp.size <= 0) throw new Error('nameImports.size <= 0')
+        return Array.from(temp)
+      })
+      .then(nameImports => {
+        const temp = nameImports.map(name => this
+          .checkExistsPromise(`${this.nameLists.keys()[0]}/${name}`)
+          .catch(()=>{})
+
+        )
+
+
+      })
+
+  }
+}
+
+function factoryParsePromise
+  (sources: number | Map<any, any> | Array<any> | Set<any>):
+  (this: FinderPathFromNames, nameImports: string[]) => Promise<Set<string>>
+{
+  const checkExistsPromise = (absolutePath: string, fsConstant = fsConstants.F_OK) => {
+    return fsPromises.access(path.resolve(`src/${absolutePath}`), fsConstant)
+  }
+  const getImportsFrom = (absolutePath: string): string[] => {
+    return require(path.resolve(`src/${absolutePath}`))
+  }
+  function parseWithoutSource(this: FinderPathFromNames, nameImports: string[]) {
+    return Promise.resolve(new Set() as Set<string>)
+  }
+  function parseOneSource(this: FinderPathFromNames, nameImports: string[]) {
+    if (nameImports.length <= 0) return
+    const source = [...this.nameLists.keys()][0]
+    const nameList = this.nameLists.get(source)
+    const promises = Array.from(new Set(nameImports)).map(name => {
+      return checkExistsPromise(`${source}/${name}`)
+        .then(existsImportedDirectory => {
+          nameList.add(`${source}/${name}/${name}`)
+          return checkExistsPromise(`${source}/${name}/${this.nameImportsFile}`)
+        })
+        .then(existsAdditionalImports => getImportsFrom(`${source}/${name}/${this.nameImportsFile}`))
+        .then(additionalImports => {
+          additionalImports.forEach(importName => this.additionalNamesList.add(importName))
+          return additionalImports
+        })
+        .catch(():string[] => [])
+    })
+    return Promise.all(promises)
+      // ts(2472) если в конфиг добавить target: ES*, то полетят импорты.
+      // а без этой настройки расширение в new нельзя использовать
+      .then((additionalImports): Set<string> => new Set([].concat(...additionalImports)))
+  }
+
+  function parseTwoSource(this: FinderPathFromNames, nameImports: string[]) {
+    if (nameImports.length <= 0)
+      return Promise.resolve(new Set() as Set<string>)
+    const sources = [...this.nameLists.keys()].slice(0, 2) as [string, string]
+    const nameList = [this.nameLists.get(sources[0]), this.nameLists.get(sources[1])] as [Set<String>, Set<String>]
+    const promises = sources.map(source => {
+
+     })
+
+ }
+  function saveNamesInSourceToNameLists(this: FinderPathFromNames, nameImports: string[], source: string) {
+    const nameList = this.nameLists.get(source)
+    const saversNameImportsInSource = nameImports.map(name => {
+      return checkExistsPromise(`${source}/${name}`)
+        .catch(()=> Promise.reject([name]))
+        .then(existsImportedDirectory => {
+          nameList.add(`${source}/${name}/${name}`)
+          return checkExistsPromise(`${source}/${name}/${this.nameImportsFile}`, fsConstants.R_OK)
+        })
+        .then(existsAdditionalImports => getImportsFrom(`${source}/${name}/${this.nameImportsFile}`))
+        .catch(():string[] => [])
+    })
+    const agregaterAdditionalImports = Promise.allSettled(saversNameImportsInSource)
+      .then(allPromisesResult => {
+        return allPromisesResult.map(promiseResult => {
+          if (promiseResult.status = 'fulfilled') {
+            return (promiseResult as PromiseFulfilledResult<string[]>).value
+          } else {
+            return (promiseResult as PromiseRejectedResult).reason as string[]
+          }
+        })
+      })
+      .then(arrayOfArraysOfSources => [].concat(...arrayOfArraysOfSources) as string[])
+      .then(arrayOfSources => Array.from(this.delDublicateFrom(new Set(arrayOfSources))))
+      .catch((): string[] => [])
+    return agregaterAdditionalImports
+  }
+  function parseManySource(this: FinderPathFromNames, nameImports: string[]) {
+    if (nameImports.length <= 0)
+      return Promise.resolve(new Set() as Set<string>);
+    const promises = [...this.nameLists.keys()].reduce((promise, source): Promise<string[]> => {
+      const nextPromise = promise.then(additionalImports => {
+        if (additionalImports.length <= 0)
+          return additionalImports
+        return saveNamesInSourceToNameLists.call(this, additionalImports, source)
+      })
+      return nextPromise
+     },Promise.resolve(nameImports))
+  }
+
+  const length: number = (sources => {
+    const temp = (sources as Array<any>).length | (sources as Map<any, any> | Set<any>).size | NaN
+    if (temp > 0) return temp
+    else return 0
+  })(sources)
+  if (length < 1) return parseWithoutSource
+  else if (length <= 1) return parseOneSource
+  else if (length <= 2) return parseTwoSource
+  else if (length > 2) return parseManySource
+}
+
+
+
+export async function separateImportNames(startDirectory: string, options: AutoInputOptions) {
+  // lists for exist folder name
 const componentsPath = 'components'
 const simplePath = `${componentsPath}/simple`
 const complicatedPath = `${componentsPath}/complicated`
-
-export async function separateImportNames(startDirectory) {
-  // lists for exist folder name
   const notCheckedSimpleImportNameList: Set<string> = new Set()
   const complicatedImportNameList: Set<string> = new Set()
   const isExistsPromise = absolutePath =>{
-    return fsPromises.access(path.resolve(`src/${absolutePath}`), fsConstsnts.F_OK);
+    return fsPromises.access(path.resolve(`src/${absolutePath}`), fsConstants.F_OK);
   }
   const parseImports = (nameImports: string[]) =>
     Promise.resolve(nameImports)
@@ -38,9 +186,12 @@ export async function separateImportNames(startDirectory) {
       .catch(err => err)
   await parseImports(require(path.resolve(startDirectory, 'import.json')));
   const simpleImportNameList: Set<string> = new Set()
-  await Promise.allSettled(Array.from(notCheckedSimpleImportNameList).map(name => isExistsPromise(`${simplePath}/${name}`)
-    .then(exists => simpleImportNameList.add(name))))
+  await Promise
+    .allSettled(Array.from(notCheckedSimpleImportNameList)
+      .map(name => isExistsPromise(`${simplePath}/${name}`)
+        .then(exists => simpleImportNameList.add(name))))
     .catch(e => e);
+
   return {
     simpleImportNameList,
     complicatedImportNameList
