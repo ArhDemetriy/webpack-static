df9d757910ad8477e76dfbe16a20748ed33aae89
ArhDemetriy <ArhDemetriy@GMail.com>
2021-03-04 21:21:18 +0800

    добавил тестов немного переделал генерацию данных для тестов

--- auto-input-loader/core/__mock__/SeparaterImportNames.test.date.ts
+++ auto-input-loader/core/__mock__/SeparaterImportNames.test.date.ts
@@ -1,23 +1,18 @@
-class SeparaterImportNamesTestDate {
-  importsFileName = 'import.json'
-  fsMock = (fsMap => {
-    const fsMock: Set<string> = new Set()
-    fsMap.forEach((value, key) => {
-      value.forEach(element => fsMock.add(`${key}/${element}`))
-    })
-    return fsMock
-  })(new Map([
-    ['src/components/complicated', new Set([
+const importsFileName = 'import.json'
+const FileSistemShot = new Map([
+  ['src/components/complicated',
+    new Set([
       'complicated_and_simple_import',
-      `complicated_and_simple_import/${this.importsFileName}`,
+      `complicated_and_simple_import/${importsFileName}`,
       'not_use_complicated',
-      `not_use_complicated/${this.importsFileName}`,
+      `not_use_complicated/${importsFileName}`,
       'only_simple_import',
-      `only_simple_import/${this.importsFileName}`,
+      `only_simple_import/${importsFileName}`,
       'unical_complicated_import',
-      `unical_complicated_import/${this.importsFileName}`,
+      `unical_complicated_import/${importsFileName}`,
     ])],
-    ['src/components/simple', new Set([
+  ['src/components/simple',
+    new Set([
       'base',
       'base1',
       'base2',
@@ -26,25 +21,39 @@ class SeparaterImportNamesTestDate {
       'text',
       'text2',
     ])],
-    ['', new Set(['',])],
-  ]))
+  // ['', new Set(['',])],
+])
+class SeparaterImportNamesTestDate {
+  importsFileName = importsFileName
+  sources = [...FileSistemShot.keys()]
+  fsMock = (fsMap => {
+    const fsMock: Set<string> = new Set()
+    fsMap.forEach((value, key) => {
+      value.forEach(element => fsMock.add(`${key}/${element}`))
+    })
+    return fsMock
+  })(FileSistemShot)
   requireMock = new Map([
-    [`src/components/complicated/complicated_and_simple_import/${this.importsFileName}`, [
-      'only_simple_import',
-      'base2',
-    ]],
-    [`src/components/complicated/not_use_complicated/${this.importsFileName}`, [
-      'base4',
-      'only_simple_import',
-    ]],
-    [`src/components/complicated/only_simple_import/${this.importsFileName}`, [
-      'base',
-      'base3',
-    ]],
-    [`src/components/complicated/unical_complicated_import/${this.importsFileName}`, [
-      'not_use_complicated',
-      'base',
-    ]],
+    [`src/components/complicated/complicated_and_simple_import/${this.importsFileName}`,
+      [
+        'only_simple_import',
+        'base2',
+      ]],
+    [`src/components/complicated/not_use_complicated/${this.importsFileName}`,
+      [
+        'base4',
+        'only_simple_import',
+      ]],
+    [`src/components/complicated/only_simple_import/${this.importsFileName}`,
+      [
+        'base',
+        'base3',
+      ]],
+    [`src/components/complicated/unical_complicated_import/${this.importsFileName}`,
+      [
+        'not_use_complicated',
+        'base',
+      ]],
   ])
 }
 export {
--- auto-input-loader/core/__tests__/SeparaterImportNames.test.ts
+++ auto-input-loader/core/__tests__/SeparaterImportNames.test.ts
@@ -1,14 +1,73 @@
+import { SettingsSeparaterImportNames, ImportNamesCollection, NamesList } from '../types'
 import { SeparaterImportNames } from '../SeparaterImportNames'
-import { SeparaterImportNamesTestDate } from './SeparaterImportNames.Test.date'
-import { promises as fsPromises } from 'fs'
-jest.mock('fs')
+import { SeparaterImportNamesTestDate } from '../__mock__/SeparaterImportNames.test.date'
 
-// ;(require as unknown as jest.MockInstance<Promise<void>, [string, number]>).mockImplementation(function (absolutePath, fsConstant) {
-// return Promise.resolve()
-// })
-// ;(fsPromises.access as unknown as jest.MockInstance<Promise<void>, [string, number]>).mockImplementation(function (absolutePath, fsConstant) {
-// return Promise.resolve()
-// })
+describe('SeparaterImportNames class:', () => {
+  let separater: SeparaterImportNames
+  let dataForSeparater = new SeparaterImportNamesTestDate()
+  let separaterSettings: SettingsSeparaterImportNames = {
+    importsFilePath: `test/${dataForSeparater.importsFileName}`,
+    sources: dataForSeparater.sources,
+  }
+  beforeAll(() => {
+    separater = new SeparaterImportNames(separaterSettings);
+    (function (this: typeof separater) {
+      this.getImportsFrom = jest.fn(this.getImportsFrom)
+        .mockImplementation(function (s: string) {
+          if (!dataForSeparater.requireMock.has(s))
+            throw new Error('MOCK file not founded');
+          return dataForSeparater.requireMock.get(s)
+        })
+      this.checkExistsPromise = jest.fn(this.checkExistsPromise)
+        .mockImplementation(function (s: string) {
+          if (dataForSeparater.fsMock.has(s))
+            return Promise.resolve()
+          else
+            return Promise.reject()
+        })
+    }).call(separater)
+  })
+  it('shouldt toBe implements SeparaterImportNames', () => {
+    expect(separater).toBeDefined()
+    expect(separater).toHaveProperty('getSeparateNames')
+    expect(typeof separater.getSeparateNames).toBe('function')
+  })
+
+  describe('getSeparateNames method:', () => {
+    it('shouldt return map', () => {
+      const separateNames = separater.getSeparateNames()
+      expect(typeof separateNames).toBe('object')
+      expect(separateNames).toHaveProperty('size')
+      expect(separateNames).toHaveProperty('get')
+      expect(separateNames).toHaveProperty('set')
+      expect(separateNames).toHaveProperty('has')
+    })
+    it('shouldt return not empty map', () => {
+      expect(separater.getSeparateNames().size).toBeGreaterThanOrEqual(0)
+    })
+    it('shouldt return map consist value:Set<string>', () => {
+      const valuesOfSeparateNames = [...separater.getSeparateNames().values()]
+      expect(valuesOfSeparateNames.length).toBeGreaterThanOrEqual(0)
+      // expect(typeof valuesOfSeparateNames).toBe('object')
+      // expect(valuesOfSeparateNames).toHaveProperty('size')
+      // expect(valuesOfSeparateNames).toHaveProperty('get')
+      // expect(valuesOfSeparateNames).toHaveProperty('set')
+      // expect(valuesOfSeparateNames).toHaveProperty('has')
+    })
+
+
+
+
+
+
+
+
+
+  })
+
+
+})
+
 
 
 
