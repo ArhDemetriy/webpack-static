1abd96485fc99b679865e498910ff1145634760b
ArhDemetriy <ArhDemetriy@GMail.com>
2021-03-06 22:51:07 +0800

    add tests mocked methods for partitioner

--- auto-input-loader/core/__tests__/PartitionerImportNames.test.ts
+++ auto-input-loader/core/__tests__/PartitionerImportNames.test.ts
@@ -1,71 +1,81 @@
 import { SettingsPartitionerImportNames, ImportNamesCollection, NamesList } from '../types'
-import { PartitionerImportNames } from '../PartitionerImportNames'
+import { PartitionerImportNames } from '../__mock__/PartitionerImportNames'
 import { PartitionerImportNamesTestDate } from '../__mock__/PartitionerImportNames.test.date'
-
 describe('PartitionerImportNames class:', () => {
   let partitioner: PartitionerImportNames
-  let dataForPartitioner = new PartitionerImportNamesTestDate()
-  let partitionerSettings: SettingsPartitionerImportNames = {
-    importsFilePath: `test/${dataForPartitioner.importsFileName}`,
-    sources: dataForPartitioner.sources,
-  }
+  let dataForPartitioner : PartitionerImportNamesTestDate
+  let partitionerSettings: SettingsPartitionerImportNames;
   beforeAll(() => {
+    jest.clearAllMocks()
+    dataForPartitioner = new PartitionerImportNamesTestDate()
+    partitionerSettings = PartitionerImportNamesTestDate.partitionerSettings()
+  })
+  beforeEach(() => {
+    partitioner = new PartitionerImportNames(partitionerSettings)
+  })
+  afterEach(function (this: typeof partitioner) {
+    jest.clearAllMocks()
+  })
+})
+
+describe('PartitionerImportNames class it ones created:', () => {
+  let partitioner: PartitionerImportNames
+  let dataForPartitioner = new PartitionerImportNamesTestDate()
+  let partitionerSettings = PartitionerImportNamesTestDate.partitionerSettings();
+  beforeAll(() => {
+    jest.clearAllMocks()
     partitioner = new PartitionerImportNames(partitionerSettings);
-    (function (this: typeof partitioner) {
-      this.getImportsFrom = jest.fn(this.getImportsFrom)
-        .mockImplementation(function (s: string) {
-          if (!dataForPartitioner.requireMock.has(s))
-            throw new Error('MOCK file not founded');
-          return dataForPartitioner.requireMock.get(s)
-        })
-      this.checkExistsPromise = jest.fn(this.checkExistsPromise)
-        .mockImplementation(function (s: string) {
-          if (dataForPartitioner.fsMock.has(s))
-            return Promise.resolve()
-          else
-            return Promise.reject()
-        })
-    }).call(partitioner)
   })
   it('shouldt toBe implements PartitionerImportNames', () => {
     expect(partitioner).toBeDefined()
     expect(partitioner).toHaveProperty('getPartitionedNames')
     expect(typeof partitioner.getPartitionedNames).toBe('function')
   })
-
   describe('getPartitionedNames method:', () => {
     it('shouldt return map', () => {
       const partitionedNames = partitioner.getPartitionedNames()
       expect(typeof partitionedNames).toBe('object')
-      expect(partitionedNames).toHaveProperty('size')
       expect(partitionedNames).toHaveProperty('get')
       expect(partitionedNames).toHaveProperty('set')
+      expect(partitionedNames).toHaveProperty('size')
       expect(partitionedNames).toHaveProperty('has')
     })
     it('shouldt return not empty map', () => {
-      expect(partitioner.getPartitionedNames().size).toBeGreaterThanOrEqual(0)
+      expect(partitioner.getPartitionedNames().size).toBeGreaterThanOrEqual(1)
     })
-    it('shouldt return map consist value:Set<string>', () => {
+    it('shouldt return map consist value:Set', () => {
       const valuesOfPartitionedNames = [...partitioner.getPartitionedNames().values()]
-      expect(valuesOfPartitionedNames.length).toBeGreaterThanOrEqual(0)
-      // expect(typeof valuesOfPartitionedNames).toBe('object')
-      // expect(valuesOfPartitionedNames).toHaveProperty('size')
-      // expect(valuesOfPartitionedNames).toHaveProperty('get')
-      // expect(valuesOfPartitionedNames).toHaveProperty('set')
-      // expect(valuesOfPartitionedNames).toHaveProperty('has')
+      expect(valuesOfPartitionedNames.length).toBeGreaterThanOrEqual(1)
+      expect(typeof valuesOfPartitionedNames[0]).toBe('object')
+      expect(valuesOfPartitionedNames[0]).toHaveProperty('add')
+      expect(valuesOfPartitionedNames[0]).toHaveProperty('size')
+      expect(valuesOfPartitionedNames[0]).toHaveProperty('has')
     })
-
-
-
-
-
-
-
-
-
   })
-
-
+  describe('fierstStep method:', function (this: typeof partitioner) {
+    const fS = () => partitioner.fierstStep
+    it('shouldt called and returned onse', () => {
+      expect(fS()).toBeCalled()
+      expect(fS()).toBeCalledTimes(1)
+      expect(fS()).toReturnTimes(1)
+    })
+    it('shouldt return string[] from initial imports file', () => {
+      expect(fS()).nthCalledWith(1, partitionerSettings.importsFilePath)
+      const initialImports = dataForPartitioner.requireMock.get(partitionerSettings.importsFilePath)
+      expect(fS()).nthReturnedWith(1, expect.arrayContaining(initialImports))
+    })
+    it('shouldt call getImportsFrom with self input', () => {
+      expect(partitioner.getImportsFrom).toBeCalled()
+      expect(partitioner.getImportsFrom).toBeCalledWith(partitionerSettings.importsFilePath)
+    })
+  })
+  describe('getImportsFrom method:', function (this: typeof partitioner) {
+    const gif = () => partitioner.getImportsFrom
+    it('shouldt be called and returned after create class ', () => {
+      expect(gif()).toBeCalled()
+      expect(gif()).toReturn()
+    })
+  })
 })
 
 
