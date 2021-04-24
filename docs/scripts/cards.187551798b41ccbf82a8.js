df1490d18e4c3276451069553f821f16d7988c88
Demetriy <ArhDemetriy@GMail.com>
2021-04-24 13:59:40 +0800

    init page

---
+++ docs/scripts/cards.187551798b41ccbf82a8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[12],{7412:function(){}},function(n){"use strict";var u=function(u){return n(n.s=u)};n.O(0,[912,70,612,683],(function(){return u(1202),u(7412)}));n.O()}]);
--- src/pages/index/index.js
+++
@@ -1,8 +0,0 @@
-import '@layouts/base.js'
-import './imports.js'
-import './imports.ts'
-import './index.scss'
-
-global.myAversome = function () {
-  console.log('gottlich!')
-}
