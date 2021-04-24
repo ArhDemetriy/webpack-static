7f43019b38dc93a936a008dd48f6a68fc586cfdd
Demetriy <ArhDemetriy@GMail.com>
2021-04-24 16:40:31 +0800

    eslint fix

--- src/components/simple/base/base.ts
+++ src/components/simple/base/base.ts
@@ -1,13 +1,13 @@
-import "air-datepicker/dist/css/datepicker.min.css"
-import "air-datepicker/dist/js/datepicker.min.js"
+import 'air-datepicker/dist/css/datepicker.min.css';
+import 'air-datepicker/dist/js/datepicker.min.js';
 
-const datepiker: HTMLDivElement = document.getElementsByClassName('js-datepiker')[0] as HTMLDivElement
+const datepiker: HTMLDivElement = document.getElementsByClassName('js-datepiker')[0] as HTMLDivElement;
 const datepickerOptions = {
   range: true,
   toggleSelected: false,
   multipleDatesSeparator: ' - ',
 
 };
-($(datepiker) as unknown as { datepicker: (options?: any) => any }).datepicker(datepickerOptions)
-const q = $(datepiker).data('datepicker')
+($(datepiker) as unknown as { datepicker: (options?: any) => any }).datepicker(datepickerOptions);
+const q = $(datepiker).data('datepicker');
 console.log(q);
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,19 @@
 /** @type {import('node')} */
-import { Configuration, RuleSetRule, RuleSetUseItem, webpack, WebpackError , ProvidePlugin, HotModuleReplacementPlugin, } from "webpack"
+import {
+  Configuration, RuleSetRule, RuleSetUseItem, webpack, WebpackError, ProvidePlugin, HotModuleReplacementPlugin,
+} from 'webpack';
+import { readdirSync } from 'fs';
 
-import path = require('path')
-import { readdirSync } from 'fs'
-
-import { CleanWebpackPlugin } from 'clean-webpack-plugin'
-import HTMLWebpackPlugin = require('html-webpack-plugin')
-import MiniCssExtractPlugin = require('mini-css-extract-plugin')
-import OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
-import TerserWebpackPlugin = require('terser-webpack-plugin')
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 // import WebpackDevServer from "webpack-dev-server";
-import { AutoImportsPlugin } from "auto-imports-plugin";
-import * as jQuery from "jQuery";
+import { AutoImportsPlugin } from 'auto-imports-plugin';
+import * as jQuery from 'jQuery';
 
+import path = require('path');
+import HTMLWebpackPlugin = require('html-webpack-plugin');
+import MiniCssExtractPlugin = require('mini-css-extract-plugin');
+import OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+import TerserWebpackPlugin = require('terser-webpack-plugin');
 
 /**
  * generate Configuration.module
@@ -25,112 +26,127 @@ class WebpackConfigModule {
    * @return {import("webpack").Configuration['module']} Configuration.module
    */
   getModule() {
-    return this.module
+    return this.module;
   }
-  private readonly module: Configuration['module'] = { rules: [] }
-  protected readonly rules: Configuration['module']['rules'] = this.module.rules
-  protected readonly importJsonFileName: string = 'imports'
-  protected readonly isDev: boolean
+
+  private readonly module: Configuration['module'] = { rules: [] };
+
+  protected readonly rules: Configuration['module']['rules'] = this.module.rules;
+
+  protected readonly importJsonFileName: string = 'imports';
+
+  protected readonly isDev: boolean;
+
   /**
    * @param {boolean} isDev
    * @constructor
    */
   constructor(isDev: boolean) {
-    this.isDev = isDev
-    this.rules.push(this.getJsRule())
-    this.rules.push(this.getTsRule())
-    this.rules.push(this.getCssRule())
-    this.rules.push(this.getScssRule())
-    this.rules.push(this.getPugRule())
-    this.rules.push(this.getImgRule())
-    this.rules.push(this.getFontsRule())
+    this.isDev = isDev;
+    this.rules.push(this.getJsRule());
+    this.rules.push(this.getTsRule());
+    this.rules.push(this.getCssRule());
+    this.rules.push(this.getScssRule());
+    this.rules.push(this.getPugRule());
+    this.rules.push(this.getImgRule());
+    this.rules.push(this.getFontsRule());
   }
+
   protected getCssRule(): RuleSetRule {
-    const cssRule: RuleSetRule = {}
-    cssRule.test = /\.css$/
+    const cssRule: RuleSetRule = {};
+    cssRule.test = /\.css$/;
     const use: RuleSetUseItem[] = [
       MiniCssExtractPlugin.loader,
       'css-loader',
-    ]
-    cssRule.use = use
-    return cssRule
+    ];
+    cssRule.use = use;
+    return cssRule;
   }
+
   protected getScssRule(): RuleSetRule {
-    const scssRule: RuleSetRule = this.getCssRule()
+    const scssRule: RuleSetRule = this.getCssRule();
     scssRule.test = /\.s[ac]ss$/;
-    (scssRule.use as RuleSetUseItem[]).push('sass-loader')
-    return scssRule
+    (scssRule.use as RuleSetUseItem[]).push('sass-loader');
+    return scssRule;
   }
+
   protected getJsRule(): RuleSetRule {
-    const jsRule: RuleSetRule = {}
-    jsRule.test = /\.m?js$/
-    jsRule.exclude = /node_modules/
+    const jsRule: RuleSetRule = {};
+    jsRule.test = /\.m?js$/;
+    jsRule.exclude = /node_modules/;
     const use: {
       ident?: string
       loader?: string
       options?: string | { [index: string]: any }
     } = {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
-      }
-    }
-    jsRule.use = use
-    return jsRule
+        presets: ['@babel/preset-env'],
+      },
+    };
+    jsRule.use = use;
+    return jsRule;
   }
+
   protected getTsRule(): RuleSetRule {
-    const tsRule: RuleSetRule = this.getJsRule()
+    const tsRule: RuleSetRule = this.getJsRule();
     tsRule.test = /\.ts$/;
     (tsRule.use as {
       ident?: string
       loader?: string
       options?: { presets: string[], [index: string]: any }
-    }).options.presets.push('@babel/preset-typescript')
-    return tsRule
+    }).options.presets.push(
+      '@babel/preset-typescript',
+    );
+    return tsRule;
   }
+
   protected getImgRule(): RuleSetRule {
-    const imgRule: RuleSetRule = {}
-    imgRule.test = /\.(png|jpg|svg|gif|svg)$/
-    imgRule.type = 'asset/resource'
-    imgRule.exclude = path.resolve(__dirname, 'src/assets/fonts')
-    const name = this.isDev ? '[name]' : '[contenthash]'
+    const imgRule: RuleSetRule = {};
+    imgRule.test = /\.(png|jpg|svg|gif|svg)$/;
+    imgRule.type = 'asset/resource';
+    imgRule.exclude = path.resolve(__dirname, 'src/assets/fonts');
+    const name = this.isDev ? '[name]' : '[contenthash]';
     imgRule.generator = {
       filename: (options) => {
-        let dirName = 'img'
-        if ('ico' == path.basename(path.dirname(options.filename)).trim().toLowerCase()) {
-          dirName = 'ico'
+        let dirName = 'img';
+        if (path.basename(path.dirname(options.filename)).trim().toLowerCase() == 'ico') {
+          dirName = 'ico';
         }
-        return `${dirName}/${name}[ext]`
-      }
-    }
-    return imgRule
+        return `${dirName}/${name}[ext]`;
+      },
+    };
+    return imgRule;
   }
+
   protected getFontsRule(): RuleSetRule {
-    const fontsRule: RuleSetRule = {}
-    fontsRule.test = /\.(svg|ttf|eot|woff|woff2)$/
-    fontsRule.type = 'asset/resource'
-    fontsRule.include = path.resolve(__dirname, 'src/assets/fonts')
+    const fontsRule: RuleSetRule = {};
+    fontsRule.test = /\.(svg|ttf|eot|woff|woff2)$/;
+    fontsRule.type = 'asset/resource';
+    fontsRule.include = path.resolve(__dirname, 'src/assets/fonts');
     fontsRule.generator = {
       filename: (options) => {
-        const dirName = path.basename(path.dirname(options.filename)).trim()
-        return `fonts/${dirName}/[name][ext]`
-      }
-    }
-    return fontsRule
+        const dirName = path.basename(path.dirname(options.filename)).trim();
+        return `fonts/${dirName}/[name][ext]`;
+      },
+    };
+    return fontsRule;
   }
+
   protected getPugRule(): RuleSetRule {
-    const pugRule: RuleSetRule = {}
-    pugRule.test = /\.pug$/
+    const pugRule: RuleSetRule = {};
+    pugRule.test = /\.pug$/;
     pugRule.use = [
       {
         loader: 'pug-loader',
         options: {
           root: path.resolve(__dirname, 'src/components'),
-        }
+        },
       },
-    ]
-    return pugRule
+    ];
+    return pugRule;
   }
+
   protected empty() { }
 }
 /**
@@ -145,46 +161,54 @@ class WebpackConfig {
    * @returns {Configuration} WebpackConfig
    */
   getConfig() {
-    return Object.assign({}, this.config, this.initialConfig)
+    return { ...this.config, ...this.initialConfig };
   }
-  protected readonly config: Configuration = {}
-  protected readonly isDev: boolean
-  protected readonly initialConfig: Configuration
+
+  protected readonly config: Configuration = {};
+
+  protected readonly isDev: boolean;
+
+  protected readonly initialConfig: Configuration;
+
   // имя этого каталога используется в нескольких методах, потому вынес в отдельную переменную
   // name of this dir is using in many methods, so i transfer him in this var
-  protected readonly pagesDir = 'pages'
-  protected readonly pages: string[]
+  protected readonly pagesDir = 'pages';
+
+  protected readonly pages: string[];
+
   constructor(config: Configuration = {}) {
-    this.isDev = this.getIsDev()
-    this.initialConfig = config
-    this.pages = this.getDirectoriesInPages()
+    this.isDev = this.getIsDev();
+    this.initialConfig = config;
+    this.pages = this.getDirectoriesInPages();
 
     // webpack config generation
-    this.setMode()
-    this.setOptimization()
-    this.setModule()
-    this.setPlugins()
-    this.setResolve()
-    this.setOutput()
-    this.setEntry()
-    this.setTarget()
-    this.setContext()
-    this.setServe()
-    this.setDevtool()
+    this.setMode();
+    this.setOptimization();
+    this.setModule();
+    this.setPlugins();
+    this.setResolve();
+    this.setOutput();
+    this.setEntry();
+    this.setTarget();
+    this.setContext();
+    this.setServe();
+    this.setDevtool();
   }
+
   protected getIsDev() {
-    const ENV = process.env.NODE_ENV == 'development'
-    const serve = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('serve')
-    const watch = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('watch')
-    return ENV || serve || watch
+    const ENV = process.env.NODE_ENV == 'development';
+    const serve = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('serve');
+    const watch = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('watch');
+    return ENV || serve || watch;
   }
+
   protected setDevtool() {
-    if (this.isDev)
-      this.config.devtool = 'source-map';
+    if (this.isDev) { this.config.devtool = 'source-map'; }
   }
+
   protected setServe() {
     if (!process.env.npm_lifecycle_event.toLocaleLowerCase().includes('serve')) return;
-    if (!this.isDev) return
+    if (!this.isDev) return;
 
     this.config.watch = false;
     (this.config as any).devServer = {
@@ -195,11 +219,13 @@ class WebpackConfig {
       hot: true,
       index: 'index.html',
       writeToDisk: true,
-    }
+    };
   }
+
   protected setMode() {
-    this.config.mode = this.isDev ? 'development' : 'production'
+    this.config.mode = this.isDev ? 'development' : 'production';
   }
+
   protected setOptimization() {
     this.config.optimization = {
       runtimeChunk: 'single',
@@ -213,26 +239,28 @@ class WebpackConfig {
             name(module) {
               // получает имя, то есть node_modules/packageName/not/this/part.js
               // или node_modules/packageName
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
               // имена npm-пакетов можно, не опасаясь проблем, использовать
               // в URL, но некоторые серверы не любят символы наподобие @
-              return `npm.${packageName.replace('@', '')}`
+              return `npm.${packageName.replace('@', '')}`;
             },
           },
         },
       },
-    }
+    };
     if (!this.isDev) {
       this.config.optimization.minimizer = [
         new OptimizeCssAssetsWebpackPlugin(),
         new TerserWebpackPlugin(),
-      ]
+      ];
     }
   }
+
   protected setModule() {
-    const moduleGenerator = new WebpackConfigModule(this.isDev)
-    this.config.module = moduleGenerator.getModule()
+    const moduleGenerator = new WebpackConfigModule(this.isDev);
+    this.config.module = moduleGenerator.getModule();
   }
+
   protected getHTMLWebpackPluginOptions(pageName: string): HTMLWebpackPlugin.Options {
     const HWPSetup: HTMLWebpackPlugin.Options = {
       template: `${this.pagesDir}/${pageName}/${pageName}.pug`,
@@ -241,65 +269,69 @@ class WebpackConfig {
       scriptLoading: 'defer',
       title: pageName,
       showErrors: this.isDev,
-      favicon: path.resolve(__dirname, 'src/assets/favicon.png',),
+      favicon: path.resolve(__dirname, 'src/assets/favicon.png'),
       // TO DO проверить минификацию, если работает без настроек - удалить. Минорно
       // minify: 'auto',
       // TO DO выяснить как работает, настроить или удалить. Минорно
       // chunks: [pageName],
-    }
-    return HWPSetup
+    };
+    return HWPSetup;
   }
-  protected getHTMLWebpackPluginsForAllPages(): Configuration['plugins']{
-    const HWPs = this.pages.map(pageName => {
-      const options = this.getHTMLWebpackPluginOptions(pageName)
-      return new HTMLWebpackPlugin(options)
-    })
-    return HWPs
+
+  protected getHTMLWebpackPluginsForAllPages(): Configuration['plugins'] {
+    const HWPs = this.pages.map((pageName) => {
+      const options = this.getHTMLWebpackPluginOptions(pageName);
+      return new HTMLWebpackPlugin(options);
+    });
+    return HWPs;
   }
+
   protected getCleanWebpackPlugin() {
-    const options: ConstructorParameters<typeof CleanWebpackPlugin>[0] = {}
+    const options: ConstructorParameters<typeof CleanWebpackPlugin>[0] = {};
 
-    const serve = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('serve')
-    const watch = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('watch')
+    const serve = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('serve');
+    const watch = process.env.npm_lifecycle_event.toLocaleLowerCase().includes('watch');
     if (serve || watch) {
-      options.cleanStaleWebpackAssets = false
+      options.cleanStaleWebpackAssets = false;
     }
 
-    return new CleanWebpackPlugin(options)
+    return new CleanWebpackPlugin(options);
   }
-  protected setPlugins() {
 
+  protected setPlugins() {
     this.config.plugins = [].concat(
       new MiniCssExtractPlugin({
         filename: `styles/[name]${this.isDev ? '' : '.[contenthash]'}.css`,
       }),
       this.getCleanWebpackPlugin(),
       new ProvidePlugin({
-        '$': 'jQuery',
-        'jQuery': 'jQuery',
+        $: 'jQuery',
+        jQuery: 'jQuery',
       }),
       this.getHTMLWebpackPluginsForAllPages(),
 
       new AutoImportsPlugin({
-        sources: ['src/components/complicated', 'src/components/simple',],
-        startDirs: this.pages.map(dirName => path.join('src/pages', dirName)),
+        sources: ['src/components/complicated', 'src/components/simple'],
+        startDirs: this.pages.map((dirName) => path.join('src/pages', dirName)),
         basenameImportFiles: 'imports',
-        importsExprGenerators: this.getImportsExprGenerators()
-      })
-    )
+        importsExprGenerators: this.getImportsExprGenerators(),
+      }),
+    );
   }
+
   protected getImportsExprGenerators() {
-    const pugImportsExprGenerator = (importPath: string) => `include ${importPath.split('\\').join('/')}\n`
-    const jsImportsExprGenerator = (importPath: string) => `import '${importPath.split('\\').join('/')}';\n`
-    const tsImportsExprGenerator = jsImportsExprGenerator
+    const pugImportsExprGenerator = (importPath: string) => `include ${importPath.split('\\').join('/')}\n`;
+    const jsImportsExprGenerator = (importPath: string) => `import '${importPath.split('\\').join('/')}';\n`;
+    const tsImportsExprGenerator = jsImportsExprGenerator;
 
-    const importsExprGenerators: ConstructorParameters<typeof AutoImportsPlugin>['0']['importsExprGenerators'] = new Map()
-    importsExprGenerators.set('.pug', pugImportsExprGenerator)
-    importsExprGenerators.set('.js', jsImportsExprGenerator)
-    importsExprGenerators.set('.ts', tsImportsExprGenerator)
+    const importsExprGenerators: ConstructorParameters<typeof AutoImportsPlugin>['0']['importsExprGenerators'] = new Map();
+    importsExprGenerators.set('.pug', pugImportsExprGenerator);
+    importsExprGenerators.set('.js', jsImportsExprGenerator);
+    importsExprGenerators.set('.ts', tsImportsExprGenerator);
 
-    return importsExprGenerators
+    return importsExprGenerators;
   }
+
   protected setResolve() {
     const alias = {
       '@simple': path.resolve(__dirname, 'src/components/simple'),
@@ -313,46 +345,52 @@ class WebpackConfig {
 
     this.config.resolve = {
       alias,
-    }
+    };
   }
+
   protected setOutput() {
-    let filename = 'scripts/[name]'
+    let filename = 'scripts/[name]';
     if (!this.isDev) {
-      filename += '.[contenthash]'
+      filename += '.[contenthash]';
     }
-    filename += '.js'
+    filename += '.js';
 
     this.config.output = {
       filename,
       path: path.resolve(__dirname, 'dist'),
-      publicPath: './'
-    }
+      publicPath: './',
+    };
   }
+
   protected setEntry() {
-    this.config.entry = {}
+    this.config.entry = {};
     for (const pageName of this.pages) {
       this.config.entry[pageName] = [
         '@babel/polyfill',
         path.resolve(__dirname, `src/${this.pagesDir}/${pageName}/${pageName}.js`),
-      ]
+      ];
     }
   }
+
   protected setTarget() {
-    this.config.target = this.isDev ? "web" : "browserslist"
+    this.config.target = this.isDev ? 'web' : 'browserslist';
   }
+
   protected setContext() {
-    this.config.context = path.resolve(__dirname, 'src')
+    this.config.context = path.resolve(__dirname, 'src');
   }
+
   protected getDirectoriesInPages(): string[] {
     return readdirSync(path.resolve(__dirname, 'src', this.pagesDir), { withFileTypes: true })
-      .filter(d => d.isDirectory())
-      .map(d => d.name)
+      .filter((d) => d.isDirectory())
+      .map((d) => d.name);
   }
+
   // заготовка под другие настройки
   protected setQ() {
     // this.config.q =
   }
 }
 
-const webpackConfigTest = new WebpackConfig()
-export default webpackConfigTest.getConfig()
+const webpackConfigTest = new WebpackConfig();
+export default webpackConfigTest.getConfig();
